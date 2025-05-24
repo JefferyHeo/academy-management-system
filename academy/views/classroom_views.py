7afb8924d98e0caa0f102970425bf6c283855084
JefferyHeo <root@LAPTOP-09BCTFUJ.localdomain>
2025-05-25 08:13:41 +0900

    Fix : classroom 중복 생성 예외 처리 및 템플릿 수정

--- academy/views/classroom_views.py
+++ academy/views/classroom_views.py
@@ -4,22 +4,28 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from academy.forms import ClassroomForm
 from academy.models import Classroom, Student
-
+from django.db import IntegrityError
 
 
 @login_required
 def classroom_create(request):
+    error = None
     if request.method == 'POST':
         form = ClassroomForm(request.POST)
         if form.is_valid():
             classroom = form.save(commit=False)
             classroom.teacher_user = request.user
             classroom.teacher_name = request.user.username
-            classroom.save()
-            return redirect('teacher_dashboard')
+            try:
+                classroom.save()
+                return redirect('teacher_dashboard')
+            except IntegrityError:
+                error = "같은 이름의 반이 이미 존재합니다."
     else:
         form = ClassroomForm()
-    return render(request, 'academy/classroom_form.html', {'form': form})
+
+    return render(request, 'academy/classroom_form.html', {'form': form, 'error': error})
+
 
 @login_required
 def delete_classroom(request, classroom_id):
