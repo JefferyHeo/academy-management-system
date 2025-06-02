d5380f4fc2a2c90d6b27b5326c0f4b02f4db8cc9
JefferyHeo <root@LAPTOP-09BCTFUJ.localdomain>
2025-06-02 15:20:21 +0900

    urls change for domain

--- ams_project/urls.py
+++ ams_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ams/', include('academy.urls')),
+    path('', include('academy.urls')),
 ]
